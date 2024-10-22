4ca6033715867003ba87cf942aff546cdedc16e0
devdoong <devdoong@naver.com>
2024-10-23 07:57:46 +0900

    SkillDataSample

--- Assets/Data/Data.Contents.cs
+++ Assets/Data/Data.Contents.cs
@@ -44,6 +44,23 @@ namespace Data
 		[XmlAttribute]
 		public int totalExp;
 	}
+
+    [Serializable, XmlRoot("PlayerDatas")]//플레이어 데이터스 안에
+    public class PlayerDataLoader : ILoader<int, PlayerData>
+    {
+        [XmlElement("PlayerData")]//플레이어 데이터가 있다
+        public List<PlayerData> stats = new List<PlayerData>(); //플레이어에 이러한 스택들을 내가 들고있을 예정이라고 선언
+
+
+        public Dictionary<int, PlayerData> MakeDict()
+        {
+            Dictionary<int, PlayerData> dict = new Dictionary<int, PlayerData>();
+            foreach (PlayerData stat in stats)
+                dict.Add(stat.level, stat); //레벨을 키값으로 전체 플레이어 데이터를 딕셔너리에 저장
+            return dict;//딕셔너리 반환
+        }
+    }
+    #endregion
     #region MonsterData
 
     public class MonsterData
@@ -67,25 +84,50 @@ namespace Data
 
 
     }
-
     #endregion
 
-    [Serializable,XmlRoot("PlayerDatas")]//플레이어 데이터스 안에
-	public class PlayerDataLoader : ILoader<int, PlayerData>
-	{
-		[XmlElement("PlayerData")]//플레이어 데이터가 있다
-		public List<PlayerData> stats = new List<PlayerData>(); //플레이어에 이러한 스택들을 내가 들고있을 예정이라고 선언
+    #region SkillData
+    public class SkillData
+    {
+        [XmlAttribute] //얻으려는 xml데이터안에 이러한 데이터들이 들어가있음을 알려줘야함 
+        public int templateID;
+
+        [XmlAttribute(AttributeName = "type")]
+        //public string skillTypeStr;
+        public Define.SkillType skillTupe = Define.SkillType.None;
+
+        [XmlAnyAttribute]
+        public int nextID;
+        public int prevID = 0; //todo
+
+        [XmlAttribute]
+        public string Prefab;
 
 
-		public Dictionary<int, PlayerData> MakeDict()
-		{
-			Dictionary<int, PlayerData> dict = new Dictionary<int, PlayerData>();
-			foreach (PlayerData stat in stats)
-				dict.Add(stat.level, stat); //레벨을 키값으로 전체 플레이어 데이터를 딕셔너리에 저장
-			return dict;//딕셔너리 반환
-		}
-	}
-	#endregion
+        //아주많이
+        [XmlAttribute]
+        public int damage; 
+    }
 
-	
+    [Serializable, XmlRoot("SkillDatas")]//플레이어 데이터스 안에
+    public class SkillDataLoader : ILoader<int, SkillData>
+    {
+        [XmlElement("SkillData")]//플레이어 데이터가 있다
+        public List<SkillData> skills = new List<SkillData>(); //플레이어에 이러한 스택들을 내가 들고있을 예정이라고 선언
+
+
+        public Dictionary<int, SkillData> MakeDict()
+        {
+            Dictionary<int, SkillData> dict = new Dictionary<int, SkillData>();
+            foreach (SkillData skill in skills)
+                dict.Add(skill.templateID, skill); //레벨을 키값으로 전체 플레이어 데이터를 딕셔너리에 저장
+            return dict;//딕셔너리 반환
+        }
+    }
+    #endregion
+
+
+
+
+
 }
